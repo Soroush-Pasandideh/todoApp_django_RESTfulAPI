2144a067c8a98e8ed076d6e64b394803489245d6
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-30 15:55:18 +0330

    change function based view to class based view.

--- todo/views.py
+++ todo/views.py
@@ -2,19 +2,12 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from todo.models import Task
 from todo.serializers import TaskSerializer
 
 
-@api_view(['GET', 'POST'])
-def task_list(request):
-    if request.method == 'GET':
-        queryset = Task.objects.all()
-        serializer = TaskSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = TaskSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
