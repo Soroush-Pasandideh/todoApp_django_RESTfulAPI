0aece17b3fc046d7d2a46e49d5d94ccd7bb898c5
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-30 15:46:32 +0330

    add urls to show end point.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -12,3 +13,8 @@ def task_list(request):
         queryset = Task.objects.all()
         serializer = TaskSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
