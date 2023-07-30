c822205e02d240b648a22ee811910edae00d02d0
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-30 12:37:55 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todoApp_django_RESTfulAPI
+ developed version of todo app using rest_framework and RESFful API.
---
+++ todoApp_django_RESTfulAPI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for todoApp_django_RESTfulAPI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoApp_django_RESTfulAPI.settings')
+
+application = get_asgi_application()
