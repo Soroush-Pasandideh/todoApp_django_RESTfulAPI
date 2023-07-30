b21d6578a045bcadf99327d9490cfe1867e5a7c9
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-30 17:15:53 +0330

    some little changes.

--- todo/serializers.py
+++ todo/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from todo.models import Task
 
 
-class TaskSerializer(serializers.Serializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['user', 'title', 'description', 'completed', 'created_at', 'category']
