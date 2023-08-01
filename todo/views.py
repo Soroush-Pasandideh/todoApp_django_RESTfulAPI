c311f1232622e8312db464da398876d16845934e
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-08-01 19:49:56 +0330

    add filter on category.

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 from todo.models import Task, Category
 from todo.serializers import TaskSerializer, CategorySerializer
@@ -11,6 +12,8 @@ from todo.serializers import TaskSerializer, CategorySerializer
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category_id']
 
 
 class CategoryViewSet(ModelViewSet):
