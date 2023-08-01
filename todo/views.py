2e0f799889849d1fb2f29ae224deabf56140e094
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-08-01 20:54:39 +0330

    adding search filter feature for tasks.

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from todo.filters import TaskFilter
@@ -13,8 +14,9 @@ from todo.serializers import TaskSerializer, CategorySerializer
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = TaskFilter
+    search_fields = ['title', 'description', 'category__title']
 
 
 class CategoryViewSet(ModelViewSet):
