5c50764e5b70d4a6ba269789a78fc4928f767f2f
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-08-01 20:58:40 +0330

    adding ordering filter feature for tasks.

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from todo.filters import TaskFilter
@@ -14,9 +14,10 @@ from todo.serializers import TaskSerializer, CategorySerializer
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = TaskFilter
     search_fields = ['title', 'description', 'category__title']
+    ordering_fields = ['title', 'completed', 'created_at']
 
 
 class CategoryViewSet(ModelViewSet):
